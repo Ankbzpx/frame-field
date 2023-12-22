fix gradient flow

gradient_flow.py
@@ -1,6 +1,6 @@
 import numpy as np
 import jax
-from jax import numpy as jnp, vmap, jit, grad
+from jax import numpy as jnp, vmap, jit, grad, jacfwd
 from common import normalize
 from sh_representation import oct_polynomial_sh4, oct_polynomial_sh4_unit_norm, oct_polynomial_zonal, oct_polynomial_zonal_unit_norm, oct_polynomial, sh4_canonical
 from loss import double_well_potential
@@ -18,7 +18,7 @@ def decompose(n, v):
 def vis_gradient(VN, gradient, local_minimums, scale=1e-2):
     gradient_para, gradient_orth = vmap(decompose)(VN, gradient)
     grad_vis_len, grad_para_vis_len, grad_orth_vis_len = jax.tree_map(
-        lambda x: scale * len(VN) * jnp.linalg.norm(x, axis=1).max(),
+        lambda x: scale * jnp.linalg.norm(x, axis=1).max(),
         [gradient, gradient_para, gradient_orth])
 
     ps.init()
@@ -48,38 +48,41 @@ if __name__ == '__main__':
     VN = vmap(normalize)(np.random.randn(100000, 3))
 
     @jit
-    def loss_basis(VN, basis):
-        dps = jnp.einsum('ij,bi->bj', basis, vmap(normalize)(VN))
-        return double_well_potential(jnp.abs(dps)).sum(-1).mean()
+    def loss_basis(VN):
+        dps = jnp.einsum('ij,i->j', R, vmap(normalize)(VN))
+        return double_well_potential(jnp.abs(dps)).sum(-1)
 
     @jit
-    def loss_sh4(VN, sh4):
-        return vmap(oct_polynomial_sh4, in_axes=[0, None])(VN, sh4).mean()
+    def loss_sh4(VN):
+        return oct_polynomial_sh4(VN)
 
     @jit
-    def loss_sh4_unit_norm(VN, sh4):
-        return vmap(oct_polynomial_sh4_unit_norm,
-                    in_axes=[0, None])(VN, sh4).mean()
+    def loss_sh4_unit_norm(VN):
+        return oct_polynomial_sh4_unit_norm(VN)
 
     @jit
-    def loss_poly(VN, R):
-        return vmap(oct_polynomial, in_axes=[0, None])(VN, R).mean()
+    def loss_poly(VN):
+        return oct_polynomial(VN, R)
 
     @jit
-    def loss_zonal(VN, R):
-        return vmap(oct_polynomial_zonal, in_axes=[0, None])(VN, R).mean()
+    def loss_zonal(VN):
+        return oct_polynomial_zonal(VN, R)
 
     @jit
-    def loss_zonal_unit_norm(VN, R):
-        return vmap(oct_polynomial_zonal_unit_norm,
-                    in_axes=[0, None])(VN, R).mean()
+    def loss_zonal_unit_norm(VN):
+        return oct_polynomial_zonal_unit_norm(VN, R)
 
     # Note the gradient of both `loss_basis` and `loss_sh4_unit_norm` lies on the sphere, but with opposite source and sink
-    # gradient = -grad(loss_basis)(VN, R)
-    # gradient = grad(loss_sh4)(VN, sh4)
-    # gradient = grad(loss_sh4_unit_norm)(VN, sh4)
-    # gradient = grad(loss_poly)(VN, R)
-    # gradient = grad(loss_zonal)(VN, R)
-    gradient = grad(loss_zonal_unit_norm)(VN, R)
+    # gradient = -vmap(grad(loss_basis))(VN)
+    # gradient = vmap(grad(loss_sh4))(VN)
+    # gradient = vmap(grad(loss_sh4_unit_norm))(VN)
+    # gradient = vmap(grad(loss_poly))(VN)
+    # gradient = vmap(grad(loss_zonal))(VN)
+    gradient = vmap(grad(loss_zonal_unit_norm))(VN)
+    loss = vmap(loss_zonal_unit_norm)(VN)
+
+    ic(gradient.shape)
+    ic(loss.max())
+    ic(loss.min())
 
     vis_gradient(VN, gradient, local_minimums)