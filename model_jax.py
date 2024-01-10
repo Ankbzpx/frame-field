ec042eb09519b742af885550276353ff69d34a4c
Ruichen Zheng <ankbzpx@hotmail.com>
2024-01-10 16:54:10 +0800

    update lip with sine

--- model_jax.py
+++ model_jax.py
@@ -19,7 +19,12 @@ class MLP(eqx.Module):
         pass
 
     def single_call(self, x, z):
-        pass
+        x = jnp.hstack([self.input_scale * x, z])
+        for i in range(len(self.layers)):
+            x = self.layers[i](x)
+            if i != len(self.layers) - 1:
+                x = getattr(jax.nn, self.activation)(x)
+        return x
 
     def single_call_split(self, x, z):
         x = self.single_call(x, z)
@@ -90,18 +95,12 @@ class Linear(eqx.Module):
                  in_features: int,
                  out_features: int,
                  key: jax.random.PRNGKey,
-                 xavier_init: bool = False,
-                 siren_first_init: bool = False):
+                 xavier_init: bool = False):
 
         if xavier_init:
             self.W = jax.random.uniform(
                 key, (out_features, in_features), minval=-1.,
                 maxval=1.) * jnp.sqrt(6. / (in_features + out_features))
-        elif siren_first_init:
-            # SIREN with omega_0=1
-            self.W = jax.random.uniform(key, (out_features, in_features),
-                                        minval=-1.,
-                                        maxval=1.) / in_features
         else:
             self.W = jax.random.normal(
                 key, (out_features, in_features)) * jnp.sqrt(2. / in_features)
@@ -138,14 +137,6 @@ class StandardMLP(MLP):
             Linear(hidden_features, hidden_features, keys[i + 1], xavier_init)
             for i in range(hidden_layers)
         ] + [Linear(hidden_features, out_features, keys[-1], xavier_init)]
-
-    def single_call(self, x, z):
-        x = jnp.hstack([self.input_scale * x, z])
-        for i in range(len(self.layers)):
-            x = self.layers[i](x)
-            if i != len(self.layers) - 1:
-                x = getattr(jax.nn, self.activation)(x)
-        return x
 
 
 class ResMLP(MLP):
@@ -192,79 +183,7 @@ class ResMLP(MLP):
         return x
 
 
-# Modified from: https://github.com/ml-for-gp/jaxgptoolbox/blob/main/demos/lipschitz_mlp/model.py
-class LipLinear(Linear):
-    W: Array
-    b: Array
-    c: Array
-
-    def __init__(self,
-                 in_features: int,
-                 out_features: int,
-                 key: jax.random.PRNGKey,
-                 xavier_init: bool = False,
-                 siren_first_init: bool = False):
-        super().__init__(in_features, out_features, key, xavier_init,
-                         siren_first_init)
-        self.c = jnp.max(jnp.sum(jnp.abs(self.W), axis=1))
-
-    # L-infinity weight normalization
-    def weight_normalization(self, W, softplus_c):
-        absrowsum = jnp.sum(jnp.abs(W), axis=1)
-        scale = jnp.minimum(1.0, softplus_c / absrowsum)
-        return W * scale[:, None]
-
-    def __call__(self, x):
-        return self.weight_normalization(self.W, jax.nn.softplus(
-            self.c)) @ x + self.b
-
-
-class LipMLP(MLP):
-    layers: list[eqx.Module]
-    activation: str
-    input_scale: float
-
-    def __init__(self,
-                 in_features: int,
-                 hidden_features: int,
-                 hidden_layers: int,
-                 out_features: int,
-                 key: jax.random.PRNGKey,
-                 activation='tanh',
-                 input_scale: float = 1,
-                 **kwargs):
-        keys = jax.random.split(key, hidden_layers + 2)
-
-        xavier_init = activation == 'tanh'
-        siren_first_init = activation == 'sin'
-        self.activation = activation
-        self.input_scale = input_scale
-
-        self.layers = [
-            LipLinear(in_features, hidden_features, keys[0], xavier_init,
-                      siren_first_init)
-        ] + [
-            LipLinear(hidden_features, hidden_features, keys[i + 1],
-                      xavier_init) for i in range(hidden_layers)
-        ] + [LipLinear(hidden_features, out_features, keys[-1], xavier_init)]
-
-    def single_call(self, x, z):
-        x = jnp.hstack([self.input_scale * x, z])
-        for i in range(len(self.layers)):
-            x = self.layers[i](x)
-            if i != len(self.layers) - 1:
-                x = getattr(jax.nn, self.activation)(x)
-        return x
-
-    # Lipschitz loss
-    # Reference: https://github.com/ml-for-gp/jaxgptoolbox/blob/7048aada5db1e6603a3d13fb1bc1ee2c61762985/demos/lipschitz_mlp/model.py#L82
-    def get_aux_loss(self):
-        loss_lip = 1.0
-        for layer in self.layers:
-            loss_lip = loss_lip * jax.nn.softplus(layer.c)
-        return loss_lip
-
-
+# This can in theory be merged with `Linear`, but we rely `omega_0` for lr filtering
 class SineLayer(eqx.Module):
     W: Array
     b: Array
@@ -290,11 +209,12 @@ class SineLayer(eqx.Module):
         self.b = jnp.zeros(out_features)
 
     def __call__(self, x):
-        return jnp.sin(self.omega_0 * (self.W @ x + self.b))
+        return self.omega_0 * (self.W @ x + self.b)
 
 
 class Siren(MLP):
     layers: list[eqx.Module]
+    activation: str
     input_scale: float
 
     def __init__(self,
@@ -309,11 +229,7 @@ class Siren(MLP):
                  **kwargs):
         keys = jax.random.split(key, hidden_layers + 2)
         self.input_scale = input_scale
-
-        # Section 3.2
-        # For [-1, 1], first_omega_0 span it to [-30, 30]
-        # Here to scale periods back
-        first_omega_0 = first_omega_0 / input_scale
+        self.activation = 'sin'
 
         self.layers = [
             SineLayer(in_features,
@@ -328,11 +244,119 @@ class Siren(MLP):
                       omega_0=hidden_omega_0) for i in range(hidden_layers)
         ] + [Linear(hidden_features, out_features, keys[-1], False)]
 
-    def single_call(self, x, z):
-        x = jnp.hstack([self.input_scale * x, z])
-        for i in range(len(self.layers)):
-            x = self.layers[i](x)
-        return x
+
+# Modified from: https://github.com/ml-for-gp/jaxgptoolbox/blob/main/demos/lipschitz_mlp/model.py
+class LipLinear(Linear):
+    W: Array
+    b: Array
+    c: Array
+
+    def __init__(self,
+                 in_features: int,
+                 out_features: int,
+                 key: jax.random.PRNGKey,
+                 xavier_init: bool = False):
+        super().__init__(in_features, out_features, key, xavier_init)
+        self.c = jnp.max(jnp.sum(jnp.abs(self.W), axis=1))
+
+    # L-infinity weight normalization
+    def weight_normalization(self, W, softplus_c):
+        absrowsum = jnp.sum(jnp.abs(W), axis=1)
+        scale = jnp.minimum(1.0, softplus_c / absrowsum)
+        return W * scale[:, None]
+
+    def __call__(self, x):
+        return self.weight_normalization(self.W, self.lipschitz()) @ x + self.b
+
+    def lipschitz(self):
+        return jax.nn.softplus(self.c)
+
+
+class LipSineLayer(SineLayer):
+    c: Array
+    c_scale: float
+
+    def __init__(self,
+                 in_features: int,
+                 out_features: int,
+                 key: jax.random.PRNGKey,
+                 is_first: bool = False,
+                 omega_0: float = 30):
+        super().__init__(in_features, out_features, key, is_first, omega_0)
+        if is_first:
+            self.c = jnp.max(jnp.sum(jnp.abs(self.W), axis=1))
+            self.c_scale = 1.0
+        else:
+            self.c = jnp.max(jnp.sum(jnp.abs(self.W), axis=1)) * omega_0
+            self.c_scale = 1.0 / omega_0
+
+    # L-infinity weight normalization
+    def weight_normalization(self, W, softplus_c):
+        absrowsum = jnp.sum(jnp.abs(W), axis=1)
+        scale = jnp.minimum(1.0, softplus_c / absrowsum)
+        return W * scale[:, None]
+
+    def __call__(self, x):
+        return self.omega_0 * (self.weight_normalization(
+            self.W, self.c_scale * jax.nn.softplus(self.c)) @ x + self.b)
+
+    def lipschitz(self):
+        return jax.nn.softplus(self.c)
+
+
+class LipMLP(MLP):
+    layers: list[eqx.Module]
+    activation: str
+    input_scale: float
+
+    def __init__(self,
+                 in_features: int,
+                 hidden_features: int,
+                 hidden_layers: int,
+                 out_features: int,
+                 key: jax.random.PRNGKey,
+                 first_omega_0: float = 1,
+                 hidden_omega_0: float = 30,
+                 activation='tanh',
+                 input_scale: float = 1,
+                 **kwargs):
+        keys = jax.random.split(key, hidden_layers + 2)
+
+        self.activation = activation
+        self.input_scale = input_scale
+
+        if activation != 'sin':
+            xavier_init = activation == 'tanh'
+            self.layers = [
+                LipLinear(in_features, hidden_features, keys[0], xavier_init)
+            ] + [
+                LipLinear(hidden_features, hidden_features, keys[i + 1],
+                          xavier_init) for i in range(hidden_layers)
+            ] + [
+                LipLinear(hidden_features, out_features, keys[-1], xavier_init)
+            ]
+        else:
+            self.layers = [
+                LipSineLayer(in_features,
+                             hidden_features,
+                             keys[0],
+                             is_first=True,
+                             omega_0=first_omega_0)
+            ] + [
+                LipSineLayer(hidden_features,
+                             hidden_features,
+                             keys[i + 1],
+                             omega_0=hidden_omega_0)
+                for i in range(hidden_layers)
+            ] + [LipLinear(hidden_features, out_features, keys[-1], False)]
+
+    # Lipschitz loss
+    # Reference: https://github.com/ml-for-gp/jaxgptoolbox/blob/7048aada5db1e6603a3d13fb1bc1ee2c61762985/demos/lipschitz_mlp/model.py#L82
+    def get_aux_loss(self):
+        loss_lip = 1.0
+        for layer in self.layers:
+            loss_lip = loss_lip * layer.lipschitz()
+        return loss_lip
 
 
 class MLPComposer(MLP):
