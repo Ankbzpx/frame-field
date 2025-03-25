2d5969f5dcf67b02bce81156a351494ca2460da5
Ruichen Zheng <ankbzpx@hotmail.com>
2025-03-25 18:09:11 +0800

    proj_n scaling

--- project_n_visualization.py
+++ project_n_visualization.py
@@ -14,6 +14,7 @@ os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "0"
 from common import normalize
 from loss import cosine_similarity
 from sh_representation import (
+    Bz,
     grad,
     project_n,
     rotvec_n_to_z,
@@ -21,6 +22,7 @@ from sh_representation import (
     rotvec_to_R9,
     sh4_canonical,
     sh4_z,
+    sh4_z_4,
 )
 
 import jax
@@ -56,11 +58,23 @@ def quaternion_to_rotvec(q):
     return rot.as_rotvec()
 
 
+@jit
+def project_z_scaled(sh4):
+    sh4_proj = sh4_z_4 + Bz.T @ (normalize(Bz @ sh4))
+    c = jnp.dot(sh4_proj, sh4)
+    return c * sh4_proj
+
+
+@jit
+def project_n_scaled(sh4, R9_zn):
+    return R9_zn.T @ project_z_scaled(R9_zn @ sh4)
+
+
 if __name__ == "__main__":
     np.random.seed(0)
 
-    # n = np.random.randn(3,)
     n = np.array([0.0, 0.0, 1.0])
+    # n = np.random.randn(3, )
     R9_zn = rotvec_to_R9(rotvec_n_to_z(n))
     sh4_test = R9_zn @ sh4_canonical
 
@@ -81,13 +95,17 @@ if __name__ == "__main__":
 
     ps.init()
     ps.register_point_cloud("sh4_viz", sh4_viz)
-    # for s in np.linspace(0.1, 1.2, 2):
+    # for s in np.linspace(0.1, 2.0, 20):
     #     sh4_tmp = sh4 * s
-    #     sh4_tmp_n = vmap(project_n, in_axes=(0, None, None))(sh4_tmp, R9_zn, 1)
-    #     loss_tmp = 1 - vmap(cosine_similarity)(sh4_tmp, sh4_tmp_n)
-    #     ps.register_point_cloud(f"sh4_viz_{s}", pcax.transform(state, sh4 * s)).add_scalar_quantity("loss_tmp", loss_tmp, enabled=True)
+    #     # sh4_tmp_n = vmap(project_n, in_axes=(0, None, None))(sh4_tmp, R9_zn, 1)
+    #     # loss_tmp = 1 - vmap(cosine_similarity)(sh4_tmp, sh4_tmp_n)
+    #     ps.register_point_cloud(f"sh4_viz_{s}", pcax.transform(state, sh4_tmp))
+    #     # .add_scalar_quantity("loss_tmp", loss_tmp, enabled=True)
 
-    NS = 10000
+    # ps.show()
+    # exit()
+
+    NS = 100
     key = jax.random.PRNGKey(0)
     r9 = jax.random.normal(key, (NS, 9))
     r9 = vmap(normalize)(r9)
@@ -96,49 +114,34 @@ if __name__ == "__main__":
     loss_l1_r9 = jnp.linalg.norm(r9 - r9_n, 1, axis=1)
     loss_l2_r9 = jnp.linalg.norm(r9 - r9_n, 2, axis=1)
 
+    r9_n_scaled = vmap(project_n_scaled, in_axes=(0, None))(r9, R9_zn)
+    loss_cosine_r9_scaled = 1 - vmap(cosine_similarity)(r9, r9_n_scaled)
+    loss_l1_r9_scaled = jnp.linalg.norm(r9 - r9_n_scaled, 1, axis=1)
+    loss_l2_r9_scaled = jnp.linalg.norm(r9 - r9_n_scaled, 2, axis=1)
+
     r9_viz = pcax.transform(state, r9)
     r9_n_viz = pcax.transform(state, r9_n)
+    r9_n_scaled_viz = pcax.transform(state, r9_n_scaled)
     curve_indices = np.arange(2 * NS).reshape(2, -1).T
     ps.register_curve_network(
         "projection", jnp.vstack([r9_viz, r9_n_viz]), curve_indices, radius=2e-3
     )
-
-    # embedding = umap.UMAP().fit_transform(sh4)
-    # ic(embedding)
-
-    # ps.init()
-    # ps_viz = ps.register_point_cloud("sh4_viz", sh4_viz)
-    # ps_viz.add_scalar_quantity("loss_cosine", loss_cosine)
-    # ps_viz.add_scalar_quantity("loss_l1", loss_l1)
-    # ps_viz.add_scalar_quantity("loss_l2", loss_l2)
+    ps.register_curve_network(
+        "projection_scaled",
+        jnp.vstack([r9_viz, r9_n_scaled_viz]),
+        curve_indices,
+        radius=2e-3,
+    )
 
     ps.register_point_cloud("r9_n_viz", r9_n_viz)
+    ps.register_point_cloud("r9_n_scaled_viz", r9_n_scaled_viz)
 
     ps_viz_r9 = ps.register_point_cloud("r9_viz", r9_viz)
     ps_viz_r9.add_scalar_quantity("loss_cosine_r9", loss_cosine_r9)
     ps_viz_r9.add_scalar_quantity("loss_l1_r9", loss_l1_r9)
     ps_viz_r9.add_scalar_quantity("loss_l2_r9", loss_l2_r9)
+
+    ps_viz_r9.add_scalar_quantity("loss_cosine_r9_scaled", loss_cosine_r9_scaled)
+    ps_viz_r9.add_scalar_quantity("loss_l1_r9_scaled", loss_l1_r9_scaled)
+    ps_viz_r9.add_scalar_quantity("loss_l2_r9_scaled", loss_l2_r9_scaled)
     ps.show()
-
-    exit()
-
-    # ps.init()
-    # ps_viz = ps.register_point_cloud("rotvecs", rotvecs)
-
-    # ps.show()
-
-    exit()
-
-    q = np.random.randn(
-        9,
-    )
-
-    R9_zn = rotvec_to_R9(rotvec_n_to_z(n))
-
-    ic(jacfwd(project_n)(q, R9_zn, 1))
-
-    # R9_zn = vmap(rotvec_to_R9)(vmap(rotvec_n_to_z)(normal))
-    # sh4_n = vmap(project_n, in_axes=(0, 0, None))(sh4, R9_zn, 1)
-
-    # ic(jnp.sqrt(5 / 12))
-    # ic(jnp.linalg.norm(((Bz.T @ Bz) @ q) / jnp.linalg.norm(Bz @ q)))
