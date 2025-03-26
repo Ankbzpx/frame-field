correct my understanding of rotation

project_n_visualization.py
@@ -11,63 +11,21 @@ import pcax
 # config.update("jax_debug_nans", True)
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "0"
 
-from common import normalize
+from common import normalize, super_fibonacci
 from loss import cosine_similarity
 from sh_representation import (
-    Bz,
-    grad,
     project_n,
+    project_n_scaled,
+    quaternion_to_rotvec,
     rotvec_n_to_z,
-    rotvec_n_to_z_raw,
     rotvec_to_R9,
     sh4_canonical,
-    sh4_z,
-    sh4_z_4,
 )
 
 import jax
-from jax import hessian, jacfwd, jit, numpy as jnp, vmap
+from jax import numpy as jnp, vmap
 
-from icecream import ic
 import polyscope as ps
-
-
-def super_fibonacci(n):
-    phi = np.sqrt(2.0)
-    psi = 1.533751168755204288118041
-
-    Q = np.empty(shape=(n, 4), dtype=float)
-
-    for i in range(n):
-        s = i + 0.5
-        r = np.sqrt(s / n)
-        R = np.sqrt(1.0 - s / n)
-        alpha = 2.0 * np.pi * s / phi
-        beta = 2.0 * np.pi * s / psi
-        Q[i, 0] = r * np.sin(alpha)
-        Q[i, 1] = r * np.cos(alpha)
-        Q[i, 2] = R * np.sin(beta)
-        Q[i, 3] = R * np.cos(beta)
-
-    return Q
-
-
-@jit
-def quaternion_to_rotvec(q):
-    rot = jax.scipy.spatial.transform.Rotation(q)
-    return rot.as_rotvec()
-
-
-@jit
-def project_z_scaled(sh4):
-    sh4_proj = sh4_z_4 + Bz.T @ (normalize(Bz @ sh4))
-    c = jnp.dot(sh4_proj, sh4)
-    return c * sh4_proj
-
-
-@jit
-def project_n_scaled(sh4, R9_zn):
-    return R9_zn.T @ project_z_scaled(R9_zn @ sh4)
 
 
 if __name__ == "__main__":
@@ -105,7 +63,7 @@ if __name__ == "__main__":
     # ps.show()
     # exit()
 
-    NS = 100
+    NS = 1000
     key = jax.random.PRNGKey(0)
     r9 = jax.random.normal(key, (NS, 9))
     r9 = vmap(normalize)(r9)

sh_representation.py
@@ -2,6 +2,8 @@ from common import normalize, Timer
 
 import jax
 from jax import grad, jit, numpy as jnp, value_and_grad, vmap
+import jax.scipy.spatial
+import jax.scipy.spatial.transform
 import numpy as np
 import optax
 
@@ -155,16 +157,19 @@ def R3_to_repvec(R, vn):
     return R[:, idx]
 
 
-# JAX has no logm implementation yet
+# TODO: Verify if it is still true
 # **IMPORTANT** Do NOT use it for interpolation (i.e. Karcher mean)
 # https://math.stackexchange.com/questions/1972695/converting-from-rotation-matrix-to-axis-angle-with-no-ambiguity
 @jit
 def R3_to_rotvec(R):
-    u = jnp.linalg.eigh(R)[1][:, 2]
-    theta = jnp.arccos(0.5 * (jnp.trace(R) - 1))
-    u_test = jnp.array([R[2, 1] - R[1, 2], R[0, 2] - R[2, 0], R[1, 0] - R[0, 1]])
-    dp = jnp.dot(u, u_test)
-    return jnp.where(dp > 0, theta * u, -theta * u)
+    rot = jax.scipy.spatial.transform.Rotation.from_matrix(R)
+    return rot.as_rotvec()
+
+
+@jit
+def quaternion_to_rotvec(q):
+    rot = jax.scipy.spatial.transform.Rotation.from_quat(q)
+    return rot.as_rotvec()
 
 
 # Note the phi, theta have different convention as in rendering
@@ -385,6 +390,20 @@ def project_z(sh4, xy_scale):
 @jit
 def project_n(sh4, R9_zn, xy_scale):
     return R9_zn.T @ project_z(R9_zn @ sh4, xy_scale)
+
+
+@jit
+def project_z_scaled(sh4):
+    sh4_proj = sh4_z_4 + Bz.T @ (normalize(Bz @ sh4))
+    # SH bands are odd dimensions
+    # Negative scaling can only result from reflection, i.e. O3 rather than SO3
+    c = jax.lax.stop_gradient(jnp.maximum(1e-2, jnp.dot(sh4_proj, sh4)))
+    return c * sh4_proj
+
+
+@jit
+def project_n_scaled(sh4, R9_zn):
+    return R9_zn.T @ project_z_scaled(R9_zn @ sh4)
 
 
 # Implement "On the Continuity of Rotation Representations in Neural Networks" by Zhou et al.