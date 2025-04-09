d045a0d26eaaf7c1c2a92c5e1c921f491f7be0fa
Ruichen Zheng <ankbzpx@hotmail.com>
2025-04-09 14:50:17 +0800

    new baseline

--- loss.py
+++ loss.py
@@ -75,7 +75,11 @@ def align_sh4_functional(sh4, normal):
 
 
 @jit
+def align_sh4_functional_grad_single_call(sh4, normal):
+    grad_normal = grad(oct_polynomial_sh4)(normalize(normal), normalize(sh4))
+    return jnp.linalg.norm(4 * normal - grad_normal, 2)
+
+
+@jit
 def align_sh4_functional_grad(sh4, normal):
-    normal = vmap(normalize)(normal)
-    grad_normal = vmap(grad(oct_polynomial_sh4))(normal, vmap(normalize)(sh4))
-    return vmap(jnp.linalg.norm, in_axes=(0, None))(4 * normal - grad_normal, 2)
+    return vmap(align_sh4_functional_grad_single_call)(sh4, normal)
